60cd75480cb14c5072b10a2170579a132ceebc3a
rhoffecker <rich.hoffecker@gmail.com>
2016-05-12 21:46:47 +0000

    Added Sendgrid configuration

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.deliver_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address                      => 'smtp.sendgrid.net',
+    :port                         => '587',
+    :authentication               => :plain,
+    :user_name                    => 'app50709693@heroku.com',
+    :password                     => 'quyfvpuu1688',
+    :domain                       => 'heroku.com',
+    :enable_starttls_auto         => true
+    }
